e8311167366e402094b795ab5a39b94635a7d52f
sshr0053 <sshr0053@yahoo.com>
2018-07-31 10:09:31 -0600

    set up styles and header grid

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import reducers from './reducers';
