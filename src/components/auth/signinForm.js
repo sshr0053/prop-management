a3fd790194c5bf3b1eccfddc60c18f772e04e37e
sshr0053 <sshr0053@yahoo.com>
2018-08-04 10:06:17 -0600

    login Field component, text-link component, and remaining signin styles

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
+import React, { Component } from "react";
+import { reduxForm, Field } from "redux-form";
 
-import { FormTitle } from '../formTitle';
-import { FormInput, FormButton } from '../formFields';
-import TextLink from '../TextLink';
+import { FormTitle } from "../formTitle";
+import { FormInput, FormButton } from "../formFields";
+import TextLink from "../textLink";
 
 class SigninForm extends Component {
     render() {
         return (
             <form className="sign-in-form">
-               <FormTitle className='sign-in-form__title' text='Login'/> 
+               <FormTitle className="sign-in-form__title" text="Login"/> 
                <Field 
-               className='sign-in-form__email' 
+               className="sign-in-form__email"
                placeholder="Enter Email" 
                name="email"
                type="email"
@@ -19,7 +19,7 @@ class SigninForm extends Component {
                component={FormInput} 
                />
                 <Field 
-               className='sign-in-form__password' 
+               className="sign-in-form__password" 
                placeholder="Enter Password" 
                name="password"
                type="password"
@@ -27,7 +27,7 @@ class SigninForm extends Component {
                component={FormInput} 
                />
                 <Field 
-               className='sign-in-form__login' 
+               className="sign-in-form__login" 
                name="login"
                type="submit"
                title="Login"
@@ -44,7 +44,7 @@ class SigninForm extends Component {
 }
 
 SigninForm = reduxForm({
-    form: 'signin'
+    form: "signin"
 })(SigninForm);
 
 export default SigninForm;
