6993d9226e63442e4ade31483bf265c6473379ec
sshr0053 <sshr0053@yahoo.com>
2018-08-08 10:20:46 -0600

    AUTHENTICATE USER reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
-import { reducer as form } from 'redux-form'
+import { reducer as form } from 'redux-form';
+import auth from './authReducers';
 
 const rootReducer = combineReducers({
-form
+form,
+auth
 });
 
 export default rootReducer;
